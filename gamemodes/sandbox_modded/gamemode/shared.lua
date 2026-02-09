c0cd9a967bcfeed7c05628fd04e2e66152467112
Flarky55 <60645124+Flarky55@users.noreply.github.com>
2026-02-10 01:41:38 +0600

    Added team creation placeholder

--- gamemodes/sandbox_modded/gamemode/shared.lua
+++ gamemodes/sandbox_modded/gamemode/shared.lua
@@ -4,3 +4,11 @@ DeriveGamemode( "sandbox" )
 
 loader.Shared( "player_class/player_sandbox_core.lua" )
 loader.Client( "cl_scoreboard.lua" )
+
+
+function GM:CreateTeams()
+    local OFFSET = 100
+
+    team.SetUp( 0 + OFFSET, "#team.superadmin",  Color( 200, 53, 69 ) )
+    team.SetUp( 1 + OFFSET, "#team.user",        Color( 128, 128, 128 ) )
+end

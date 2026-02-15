93d1bcdecf66cf290b48f28314cd8f7710760acc
Flarky55 <60645124+Flarky55@users.noreply.github.com>
2026-02-15 10:50:49 +0600

    Changed server settings placeholder

--- lua/core/cl_settings.lua
+++ lua/core/cl_settings.lua
@@ -1,8 +1,24 @@
-local Settings = {}
+local Settings = {
+    {
+        Name = "#core.settings.thirdperson",
+        ConVar = "cl_thirdperson_enable",
+        -- Init = 
+    },
+    {
+        Name = "#core.settings.smooth_noclip",
+        ConVar = "smooth_noclip",
+        -- Init = 
+    },
+    {
+        Name = "#core.settings.smooth_noclip_crouch_default",
+        ConVar = "smooth_noclip_crouch_default",
+        -- Init = 
+    },
+}
 
 
 hook.Add( "PopulateToolMenu", "core.settings", function()
     spawnmenu.AddToolMenuOption( "Utilities", "User", "core", "#spawnemnu.category.core", nil, nil, function( cpanel )
-
+        cpanel:CheckBox( "#core.settings.thirdperson", "cl_thirdperson_enable" )
     end )
 end )

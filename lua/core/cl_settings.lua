9d0e5d70a97017e1117d1e0ae21223a69ea23e37
Flarky55 <60645124+Flarky55@users.noreply.github.com>
2026-02-15 23:33:08 +0600

    Updated settings

--- lua/core/cl_settings.lua
+++ lua/core/cl_settings.lua
@@ -1,24 +1,66 @@
-local Settings = {
-    {
-        Name = "#core.settings.thirdperson",
-        ConVar = "cl_thirdperson_enable",
-        -- Init = 
-    },
-    {
-        Name = "#core.settings.smooth_noclip",
-        ConVar = "smooth_noclip",
-        -- Init = 
-    },
-    {
-        Name = "#core.settings.smooth_noclip_crouch_default",
-        ConVar = "smooth_noclip_crouch_default",
-        -- Init = 
-    },
-}
+local function PopulateToolMenu()
+    local DForm = vgui.GetControlTable( "DForm" )
 
+    local Settings = {
+        {
+            Name = "#core.settings.category.thirdperson",
+            {
+                Name = "#core.settings.enabled",
+                ConVar = "cl_thirdperson_enable",
+                Init = DForm.CheckBox
+            },
+        },
+        {
+            Name = "#core.settings.category.performant_render",
+            {
+                Name = "#core.settings.enabled",
+                ConVar = "performantrender_enable",
+                Init = DForm.CheckBox
+            },
+        },
+        {
+            Name = "#core.settings.category.smooth_noclip",
+            {
+                Name = "#core.settings.enabled",
+                ConVar = "smooth_noclip",
+                Init = DForm.CheckBox
+            },
+            {
+                Name = "#core.settings.smooth_noclip_crouch_default",
+                ConVar = "smooth_noclip_crouch_default",
+                Init = DForm.CheckBox
+            },
+        },
+        {
+            Name = "#core.settings.category.mute_toolgun",
+            {
+                Name = "#core.settings.cl_toolgun_sound",
+                ConVar = "cl_toolgun_sound",
+                Init = DForm.CheckBox
+            },
+            {
+                Name = "#core.settings.cl_toolgun_effects",
+                ConVar = "cl_toolgun_effects",
+                Init = DForm.CheckBox
+            },
+        },
+    }
 
-hook.Add( "PopulateToolMenu", "core.settings", function()
+    
     spawnmenu.AddToolMenuOption( "Utilities", "User", "core", "#spawnemnu.category.core", nil, nil, function( cpanel )
-        cpanel:CheckBox( "#core.settings.thirdperson", "cl_thirdperson_enable" )
+        for _, entry in ipairs( Settings ) do
+            local form = vgui.Create( "DForm", cpanel )
+            form:SetLabel( entry.Name )
+
+            cpanel:AddItem( form )
+
+            for _, setting in ipairs( entry ) do
+                setting.Init( form, setting.Name, setting.ConVar )
+            end
+        end
     end )
-end )
+end
+
+
+if vgui.Exists( "DForm" ) then PopulateToolMenu() end
+hook.Add( "PopulateToolMenu", "core.settings", PopulateToolMenu )

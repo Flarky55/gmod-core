2171273b03135835af65e8bbe1cabcdac59a86f1
Flarky55 <60645124+Flarky55@users.noreply.github.com>
2026-08-23 16:59:06 +0600

    Moved legacy commands: "leave", "keepmap"; Remove default "pm" command

--- lua/sam/modules/conred.lua
+++ lua/sam/modules/conred.lua
@@ -19,7 +19,7 @@ do
 
         :OnExecute(function(ply)
             local value = ply:GetInfoNum("cr_sam_allowtp", 1) == 1 and 0 or 1
-            
+
             ply:ConCommand("cr_sam_allowtp " .. value)
 
             ply:sam_send_message("Вы {V} телепортацию к себе.", {
@@ -27,24 +27,6 @@ do
             })
         end)
     :End()
-
-    command.new("leave")
-        :DisallowConsole()
-        :Aliases("выйти")
-
-        :Help("Выйти с сервера, но этого не надо делать")
-
-        :AddArg("text", {
-            default = "Kicked by user.",
-            hint = "причина"
-        })
-
-        :GetRestArgs()
-
-        :OnExecute(function(ply, reason)
-            ply:Kick(reason)
-        end)
-    :End() 
 
     command.new("voteclear")
         :SetCategory("Голосование")
@@ -55,14 +37,14 @@ do
             local function callback(_, result)
                 if result == "Нет" then
                     sam.player.send_message(nil, "Голосование провалилось, карта не будет очищена")
-                    
+
                     return
                 end
-    
+
                 timer.Simple( 60, game.CleanUpMap )
             end
             sam.vote.start_vote(ply, callback, "Очистить карту?", nil, "Да", "Нет")
-    
+
             sam.player.send_message(nil, "{A} начал голосование за очистку карты", {
                 A = ply
             })
@@ -73,53 +55,38 @@ end
 
 command.set_category(CAT_ADMIN)
 do
-    command.new("keepmap")
-        :SetPermission("keepmap", "admin")
-
-        :Help("Отменить смену карты.")
-
-        :OnExecute(function(ply)
-            if timer.Exists("SAM.Command.Map") then
-                timer.Remove("SAM.Command.Map")
-                sam.player.send_message(nil, "{A} отменил смену карты.", {
-                    A = ply
-                })
-            end
-        end)
-    :End() 
-
     do
         local cmd_mute, cmd_unmute = command.get_command("mute"), command.get_command("unmute")
         local cmd_gag, cmd_ungag = command.get_command("gag"), command.get_command("ungag")
-    
+
         command.new("shut")
             :SetPermission("shut", "admin")
-    
+
             :Help("Замутить в голосовом и текстовом чате")
-    
+
             :AddArg("player")
             :AddArg("length")
             :AddArg("text", {hint = "причина", optional = true, default = sam.language.get("default_reason")})
 
             :GetRestArgs()
-    
+
             :OnExecute(function(ply, targets, length, reason)
                 cmd_mute.on_execute(ply, targets, length, reason)
                 cmd_gag.on_execute(ply, targets, length, reason)
             end)
         :End()
-    
+
         command.new("unshut")
             :SetPermission("shut", "admin")
-            
+
             :Help("Размутить в голосовом и текстовом чате")
-    
+
             :AddArg("player")
-    
+
             :OnExecute(function(ply, targets)
                 cmd_unmute.on_execute(ply, targets)
                 cmd_ungag.on_execute(ply, targets)
             end)
-        :End() 
+        :End()
     end
 end
--- lua/sam/modules/core.lua
+++ lua/sam/modules/core.lua
@@ -1,13 +1,16 @@
 local ARGUMENT_NICKNAME = { default = "garry :D", hint = "nickname" }
 
 
+sam.command.remove_command( "pm" )
+
+
 sam.command.set_category( "C.O.R.E." )
 
 sam.command.new( "build" )
     :Aliases( "билд" )
     :DisallowConsole()
     :Help( "Перейти в режим Build." )
-    
+
     :OnExecute( function( ply )
         ply:SetGamemode( CORE_GAMEMODE_BUILD )
     end )
@@ -17,7 +20,7 @@ sam.command.new( "pvp" )
     :Aliases( "пвп" )
     :DisallowConsole()
     :Help( "Перейти в режим PvP." )
-    
+
     :OnExecute( function( ply )
         ply:SetGamemode( CORE_GAMEMODE_PVP )
     end )
@@ -27,28 +30,43 @@ sam.command.new( "nick" )
     :Aliases( "setnick", "name", "setname" )
     :DisallowConsole()
     :Help( "Изменить свой никнейм." )
-    
+
     :AddArg( "text", ARGUMENT_NICKNAME )
     :GetRestArgs()
-    
-    :OnExecute( function( ply, nick ) 
+
+    :OnExecute( function( ply, nick )
         ply:SetFakeNick( nick )
     end )
 :End()
 
 sam.command.new( "persistentnick" )
     :DisallowConsole()
-    :Help( 
-        "Установить постоянный никнейм.\n" .. 
-        "Этот никнейм будет отображаться при подключении и автоматически устанавливаться после захода на сервер." 
+    :Help(
+        "Установить постоянный никнейм.\n" ..
+        "Этот никнейм будет отображаться при подключении и автоматически устанавливаться после захода на сервер."
     )
-    
+
     :AddArg( "text", ARGUMENT_NICKNAME )
     :GetRestArgs()
 
     :OnExecute( function( ply, nick )
         ply:SetPersistentNick( nick )
         ply:SetFakeNick( nick )
+    end )
+:End()
+
+sam.command.new( "leave" )
+    :DisallowConsole()
+    :Help( "Выйти с сервера с кастомной причиной. Но выходить не надо." )
+
+    :AddArg( "text", {
+        default = "Kicked by user.",
+        hint = "причина"
+    } )
+    :GetRestArgs()
+
+    :OnExecute( function( ply, reason )
+        ply:Kick( reason )
     end )
 :End()
 
@@ -64,7 +82,19 @@ sam.command.new( "forcenick" )
 
     :OnExecute( function( ply, targets, nick )
         for _, target in ipairs( targets ) do
-            ply:SetFakeNick( nick )
+            target:SetFakeNick( nick )
         end
+    end )
+:End()
+
+sam.command.new( "keepmap" )
+    :SetPermission( "keepmap", "admin" )
+    :OnExecute( function( ply )
+        if not timer.Exists( "SAM.Command.Map" ) then return end
+
+        timer.Remove("SAM.Command.Map")
+        sam.player.send_message(nil, "{A} отменил смену карты.", {
+            A = ply
+        })
     end )
 :End()
